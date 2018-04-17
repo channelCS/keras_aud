5565abb4c44a57a7bcb1fed514543b0bcc87499d
Akshita Gupta <adityadvlp@gmail.com>
2018-04-17 20:52:23 +0530

    Adding python file example

--- README.md
+++ README.md
@@ -2,3 +2,50 @@
 [![license](https://img.shields.io/badge/License-MIT-brightgreen.svg)](https://github.com/channelCS/keras_aud/blob/master/LICENSE) [![dep1](https://img.shields.io/badge/Theano-0.9+-blue.svg)](http://deeplearning.net/software/theano/) [![dep2](https://img.shields.io/badge/Keras-2.1+-blue.svg)](https://keras.io/) 
 
 Neural networks audio toolkit for Keras
+
+import sys
+ka_path="e:/akshita_workspace/cc"
+sys.path.insert(0, ka_path)
+from keras_aud import aud_audio
+
+### Useful Path Definitions:
+
+| Variable        | Description                     |
+| :-------------  |:-------------                   |
+| `wav_dev_fd`    | Development audio folder        |
+| `wav_eva_fd`    | Evaluation audio folder         |
+| `dev_fd`        | Development features folder     |
+| `eva_fd`        | Evaluation features folder      |
+
+aud_audio.extract('logmel', wav_dev_fd, dev_fd,'example.yaml')
+aud_audio.extract('logmel', wav_eva_fd, eva_fd,'example.yaml')
+
+from keras_aud import aud_model
+
+### Model and Feature Parameters
+
+| Variable           | Description              | type       | Accepted values             |
+| :-------------     | :-------------           | :--------- | :---------                  |
+| `prep`             | mode to use              | `str`      | dev, eval                   |
+| `save_model`       | Whether to save model    | `bool`     |                             |
+| `model_type`       | Type of model            | `str`      | Dynamic, Functional, Static |
+| `model`            | Name of model            | `str`      | DNN, CNN, CRNN, RNN, FCRNN  |
+| `modelx`           | Name of model for saving | `str`      | Should end with `.h5`       |
+| `feature`          | Name of feature          | `str`      | mel, logmel, cqt, mfcc, zcr |
+| **Works only for Functional** | | | |
+| `dropout1`         | 1st Dropout              | `float`    |                             |
+| `act1`             | 1st Activation           | `str`      |                             |
+| `act2`             | 2nd Activation           | `str`      |                             |
+| `act3`             | 3rd Activation           | `str`      |                             |
+| `act4`             | 4th Activation           | `str`      | Only in case of DNN         |
+| **Works for all Models** | | | |
+| `input_neurons`    | Number of Neurons        | `int`      |                             |
+| `epochs`           | Number of Epochs         | `int`      |                             |
+| `batchsize`        | Batch Size               | `int`      |                             |
+| `num_classes`      | Number of classes        | `int`      |                             |
+| `filter_length`    | Size of Filter           | `int`      |                             |
+| `nb_filter`        | Number of Filters        | `int`      |                             |
+| **Feature Parameters** | | | |
+| `agg_num`          | Number of frames         | `int`      |                             |
+| `hop`              | Hop Length               | `int`      |                             |
+| `custom_check_ftr` | check for dimensions     | `bool`     | True: know dimension        |
--- model_description.py
+++ model_description.py
@@ -9,7 +9,7 @@ from keras.models import Model
 from keras.layers import Dense, Dropout, Flatten, Input
 from keras.layers import Conv2D, MaxPooling2D, Conv2DTranspose
 from keras.layers import BatchNormalization, ZeroPadding2D, GlobalAveragePooling2D
-from keras.layers import Embedding, LSTM, Concatenate, Reshape, Bidirectional
+from keras.layers import Embedding, LSTM, Reshape, Bidirectional, TimeDistributed, Permute
 from keras.models import load_model
 from keras import backend as K
 import numpy as np
@@ -100,23 +100,20 @@ def cnn_rnn(input_neurons,dimx,dimy,num_classes,nb_filter,filter_length,act1,act
                activation=act1)(main_input)
     hx = MaxPooling2D(pool_size=pool_size)(x)
     wrap= Dropout(dropout)(hx)
-    h = Flatten()(wrap)
-    wrap = Dense(input_neurons, activation=act2,name='wrap')(h)
-    b,c= kr(wrap)
-    x = Reshape((1,c))(wrap)
+    x = Permute((2,1,3))(wrap)
+    a,b,c,d= kr(x)
+    x = Reshape((b*d,c))(x)
     x = LSTM(32)(x)
     wrap= Dropout(dropout)(x)
     x = Dense(input_neurons, activation=act3)(wrap)
-
-    main_output = Dense(num_classes, activation='sigmoid', name='main_output')(wrap)
+    main_output = Dense(num_classes, activation='softmax', name='main_output')(wrap)
     model = Model(inputs=main_input, outputs=main_output)
     model.summary()
     model.compile(loss='categorical_crossentropy',
-              optimizer='adadelta',
-              metrics=['accuracy'])
+			  optimizer='adadelta',
+			  metrics=['accuracy'])
     
     return model
-
 ########################### BASIC FCRNN #################################
 def feature_cnn_rnn(input_neurons,dimx,dimy,num_classes,nb_filter,filter_length,act1,act2,act3,pool_size=(2,2),dropout=0.1):
     #Extract features using CNN and pass them as a input to LSTM
@@ -141,30 +138,27 @@ def feature_cnn_rnn(input_neurons,dimx,dimy,num_classes,nb_filter,filter_length,
 
 ############################# BASIC CBRNN #############################
 def cbrnn(input_neurons,dimx,dimy,num_classes,nb_filter,filter_length,act1,act2,act3,pool_size=(2,2),dropout=0.1):
+    #CNN with biderectional lstm
     print "CBRNN"
     main_input = Input(shape=(1,dimx,dimy))
     x = Conv2D(filters=nb_filter,
                kernel_size=filter_length,
                data_format='channels_first',
                padding='same',
-               activation=act1)(main_input)
+               activation='tanh')(main_input)
     hx = MaxPooling2D(pool_size=pool_size)(x)
     wrap= Dropout(dropout)(hx)
-
-    h = Flatten()(wrap)
-    wrap = Dense(input_neurons, activation=act2,name='wrap')(h)
-    b,c= kr(wrap)
-    x = Reshape((1,c))(wrap)
-
-    x = Bidirectional(LSTM(32))(x)
-    wrap= Dropout(dropout)(x)
-
+    x = Permute((2,1,3))(wrap)
+    a,b,c,d= kr(x)
+    x = Reshape((b*d,c))(x) 
+    w = Bidirectional(LSTM(32))(x)
+    wrap= Dropout(dropout)(w)
     main_output = Dense(num_classes, activation='sigmoid', name='main_output')(wrap)
     model = Model(inputs=main_input, outputs=main_output)
     model.summary()
     model.compile(loss='categorical_crossentropy',
-              optimizer='adam',
-              metrics=['accuracy'])
+			  optimizer='adam',
+			  metrics=['accuracy'])
     
     return model
 
